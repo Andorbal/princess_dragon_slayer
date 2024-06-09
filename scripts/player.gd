3d0865e3ec00866cb0a0630c94dddd87f381d8e5
Andrew Benz <andorbal@gmail.com>
2024-06-09 17:57:24 -0500

    Add some coyote time

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,21 +3,33 @@ extends CharacterBody2D
 
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
+const COYOTE_LENGTH = 0.025
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var is_falling = true
+var coyote_time = 0;
 
 func _physics_process(delta):
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	if is_on_floor():
+		is_falling = false
+	else:
+		if is_falling or coyote_time > COYOTE_LENGTH:
+			is_falling = true
+			coyote_time = 0
+			velocity.y += gravity * delta
+		else:
+			coyote_time += delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and not is_falling:
 		velocity.y = JUMP_VELOCITY
+		is_falling = true
+		coyote_time = 0
 
 	# Gets input direction: -1, 0, 1
 	var direction = Input.get_axis("move_left", "move_right")
